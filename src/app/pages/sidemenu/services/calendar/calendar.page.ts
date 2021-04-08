e8c0449bd9ba677cc4f3923c5bc5e4aa24909f1a
javier muñoz <x_jemd_x@hotmail.com>
2021-04-08 00:48:28 -0400

    modal calendario ahora es nuevo tab de servicios

--- src/app/pages/sidemenu/services/calendar/calendar.page.ts
+++ src/app/pages/sidemenu/services/calendar/calendar.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ActionSheetController, ModalController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { Service } from 'src/app/models/service';
 import { ApiService } from 'src/app/providers/api/api.service';
@@ -8,6 +8,7 @@ import { CalendarComponent } from 'ionic2-calendar';
 import { AuthService } from 'src/app/providers/auth/auth.service';
 import { User } from 'src/app/models/user';
 import { environment } from 'src/environments/environment';
+import { BrowserTab } from '@ionic-native/browser-tab/ngx';
 
 @Component({
   selector: 'app-calendar',
@@ -26,31 +27,80 @@ export class CalendarPage implements OnInit {
   }
   user: User
   apiUrl: string = environment.HOST + '/'
-  
+
 
   constructor(
     private modalController: ModalController,
     private api: ApiService,
-    private auth: AuthService
+    private auth: AuthService,
+    private actionSheetController: ActionSheetController,
+    private browserTab: BrowserTab
   ) { }
 
   ngOnInit() {
     this.user = this.auth.userData()
-    this.$nextServices = this.api.getServicesHistoryByDate(this.user.provider_id, dayjs(this.calendar.currentDate).format('YYYY-MM-DD'))
+    this.$nextServices = this.api.getServicesByDate(this.user.provider_id, dayjs(this.calendar.currentDate).format('YYYY-MM-DD'))
   }
 
   ngAfterViewInit() {
     document.querySelector('ion-list.event-detail-container').remove()
   }
-  
+
   async closeModal() {
     await this.modalController.dismiss()
   }
 
-  onDateSelected($event){
+  onDateSelected($event) {
     console.log($event.selectedTime)
     this.calendar.title = $event.selectedTime
-    this.$nextServices = this.api.getServicesHistoryByDate(this.user.provider_id, dayjs($event.selectedTime).format('YYYY-MM-DD'))
+    this.$nextServices = this.api.getServicesByDate(this.user.provider_id, dayjs($event.selectedTime).format('YYYY-MM-DD'))
+  }
+
+  async presentActionSheet(service) {
+    const actionSheet = await this.actionSheetController.create({
+      header: service.title + ' (' + service.client.firstname + ' ' + service.client.lastname + ')',
+      // cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Ver dirección',
+        icon: 'location-outline',
+        handler: () => {
+          let address = (service.other) ? [service.address.street, service.address.other, service.address.district, service.address.region].join('+') : [service.address.street, service.address.district, service.address.region].join('+')
+          address = address.replace(' ', '%20')
+          this.browserTab.isAvailable()
+            .then(isAvailable => {
+              if (isAvailable) {
+                this.browserTab.openUrl('https://www.google.com/maps/place/' + address);
+                console.log('Direccion', 'https://www.google.com/maps/place/' + address);
+              } else {
+                window.open('https://www.google.com/maps/place/' + address);
+              }
+            })
+            .catch(err => {
+              window.open('https://www.google.com/maps/place/' + address);
+            });
+        }
+      }, {
+        text: 'Marcar como terminado',
+        icon: 'checkmark-done-outline',
+        handler: () => {
+          console.log('Marcar como terminado');
+        }
+      }, {
+        text: 'Cancelar Servicio',
+        icon: 'ban-outline',
+        handler: () => {
+          console.log('Cancelar Servicio');
+        }
+      }, {
+        text: 'Cerrar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cerrar');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
