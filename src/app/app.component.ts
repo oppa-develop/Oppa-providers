820a0f476e1d16445bc94dd38ca493fcb167b37f
Javier Muñoz <x_jemd_x@hotmail.com>
2021-11-08 13:34:26 -0300

    se elimina backButtonEvent para mantener la app en segundo plano al salir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, QueryList, ViewChildren } from '@angular/core';
 
-import { AlertController, IonRouterOutlet, Platform } from '@ionic/angular';
+import { IonRouterOutlet, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
@@ -21,48 +21,11 @@ export class AppComponent {
     private statusBar: StatusBar,
     private screenOrientation: ScreenOrientation,
     public router: Router,
-    private alertController: AlertController,
     private autostart: Autostart
   ) {
     this.setPortrait();
     this.initializeApp();
     this.autostart.enable();
-    this.backButtonEvent();
-  }
-
-  backButtonEvent() {
-    this.platform.backButton.subscribeWithPriority(10, () => {
-      this.routerOutlets.forEach((outlet: IonRouterOutlet) => {
-        if (outlet?.canGoBack() && (this.router.url !== '/sidemenu/services/calendar' && this.router.url !== '/sidemenu/services/offered' && this.router.url !== '/sidemenu/services/history')) {
-          outlet.pop();
-        } else {
-          this.presentAlert();
-        }
-      });
-    });
-  }
-
-  async presentAlert() {
-    localStorage.setItem('createElderAccountAlert', 'done')
-    const alert = await this.alertController.create({
-      backdropDismiss: false,
-      header: 'Desea salir de la aplicación?',
-      buttons: [{
-        text: 'Cancelar',
-        role: 'cancel',
-        handler: () => {
-          console.log('no cerrar app');
-        }
-      }, {
-        text: 'Salir',
-        handler: async () => {
-          console.log('cerrando app');
-          navigator['app'].exitApp();
-        }
-      }]
-    })
-
-    await alert.present();
   }
 
   // set orientation to portrait
